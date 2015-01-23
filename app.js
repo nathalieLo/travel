a1785de2ad2bcb018f2c1eb451498967ccc9ffa3
nathalieVH <nathalie.harrold@dwp.gsi.gov.uk>
2015-01-23 11:37:54 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+
+var handlebars = require('express3-handlebars').create({ defaultLayout:'main' })
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function(req, res){
+   res.render('home', {name : "Paul"});
+});
+
+app.get('/about', function(req, res){
+	res.render('about');
+});
+
+// custom 404 page
+app.use(function(req, res){
+	res.status(404);
+	res.render('404');
+});
+
+// custom 500 page
+app.use(function(err, req, res, next){
+	console.error(err.stack);	
+	res.status(500);
+	res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+	console.log( 'nats second app' + app.get('port'));
+});
